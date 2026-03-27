b8a6927968d9dca4af5a72ddd66c4f969b9f61eb
Saeko Kokubun <pomodoro.e.spinaci@gmail.com>
2026-03-27 15:25:03 +0900

    HTML、defaultCSS、クラス追加JS作成

---
+++ js/filtering.js
@@ -0,0 +1,110 @@
+const addClass = () => {
+  const item = document.querySelectorAll('.item');
+  const sexObj = [
+    {
+      id: 1,
+      type: 'オス',
+      class: 'is-male',
+    },
+    {
+      id: 2,
+      type: 'メス',
+      class: 'is-female',
+    },
+  ];
+  const ageObj = [
+    {
+      id: 1,
+      type: '大人',
+      class: 'is-adult',
+    },
+    {
+      id: 2,
+      type: '子供',
+      class: 'is-kitten',
+    },
+  ];
+  const colorObj = [
+    {
+      id: 1,
+      type: '白',
+      class: 'is-white',
+    },
+    {
+      id: 2,
+      type: '黒',
+      class: 'is-black',
+    },
+    {
+      id: 3,
+      type: 'グレー',
+      class: 'is-gray',
+    },
+    {
+      id: 4,
+      type: '白黒',
+      class: 'is-shirokuro',
+    },
+    {
+      id: 5,
+      type: 'キジトラ',
+      class: 'is-kijitora',
+    },
+    {
+      id: 6,
+      type: 'サバトラ',
+      class: 'is-sabatora',
+    },
+    {
+      id: 7,
+      type: '茶トラ',
+      class: 'is-chatora',
+    },
+    {
+      id: 8,
+      type: '白キジ',
+      class: 'is-shirokiji',
+    },
+    {
+      id: 9,
+      type: '白サバ',
+      class: 'is-shirosaba',
+    },
+    {
+      id: 10,
+      type: '白茶',
+      class: 'is-shirocha',
+    },
+    {
+      id: 11,
+      type: '三毛',
+      class: 'is-mike',
+    },
+    {
+      id: 12,
+      type: 'サビ',
+      class: 'is-sabi',
+    },
+    {
+      id: 13,
+      type: 'その他',
+      class: 'is-other',
+    },
+  ];
+  item.forEach((elm) => {
+    // const txt = elm.textContent;
+    const sexTxt = elm.querySelector('.detailSex .detailValue').textContent;
+    const ageTxt = elm.querySelector('.detailAge .detailValue').textContent;
+    const colorTxt = elm.querySelector('.detailColor .detailValue').textContent;
+    const repAgeTxt = ageTxt
+      .replace(/[0-9]/g, '')
+      .replace('歳', '大人')
+      .replace('ヶ月', '子供');
+    const sex = sexObj.find((obj) => obj.type === sexTxt);
+    const age = ageObj.find((obj) => obj.type === repAgeTxt);
+    const color = colorObj.find((obj) => obj.type === colorTxt);
+    elm.classList.add(sex.class, age.class, color.class);
+  });
+};
+
+addClass();
